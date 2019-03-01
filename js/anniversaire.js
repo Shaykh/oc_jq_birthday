73cf4df33658d436afd9e65ca8f5bb908bfa4f74
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-03-01 20:11:05 +0000

    add fadeIn function to wish cards

--- js/anniversaire.js
+++ js/anniversaire.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
     $("#first-wish").on("click", function() {
         $("#second-wish").fadeIn(1800);
     });
+
+    $("#second-wish").on("click", function() {
+        $("#third-wish").fadeIn(1800);
+    });
+
+    $("#third-wish").on("click", function() {
+        $("#fourth-wish").fadeIn(1800);
+    });
 });
